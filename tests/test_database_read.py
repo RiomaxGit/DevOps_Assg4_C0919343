160c33193d12d3d546527ef560f5ea7e782dfe90
Akhil Antony Joseph <127351540+RiomaxGit@users.noreply.github.com>
2024-11-17 20:17:15 -0500

    Update test_database_read.py

--- tests/test_database_read.py
+++ tests/test_database_read.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 class TestDatabaseRead(unittest.TestCase):
     def setUp(self):
-        self.client = MongoClient("MONGODB_URI")  # Used for local testing. Removed it.
+        self.client = MongoClient("mongodb+srv://akhilantonyjoseph1997:5dsTWl4KxKTWbOea@vortex0.hvopo.mongodb.net/?retryWrites=true&w=majority&appName=Vortex0")  # Used for local testing. Removed it.
 
     def test_ping(self):
         """Test MongoDB connection using ping."""
